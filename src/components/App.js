4261f3139f2f922d0d81035a900ca12789df920c
distoneman <distoneman@gmail.com>
2018-12-04 22:00:42 -0700

    Finish app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+// import { ToastContainer, toast } from 'react-toastify';
 import './App.css';
 
+import Post from './Post/Post'
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
 
@@ -13,38 +15,90 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get(`https://practiceapi.devmountain.com/api/posts`)
+      .then((response) => {
+        console.log(response.data)
+        this.setState({
+          posts: response.data
+        })
+        // toast.success(`got posts`)
+        console.log('got posts')
+      })
+      .catch((error) => {
+        // toast.error(`didn't get posts`)
+        console.log(`didn't get posts`)
+      })
   }
 
-  updatePost() {
-  
+  updatePost(id, text) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then((response) => {
+        console.log('put response', response)
+        this.setState({ posts: response.data })
+      })
+      .catch((error) => {
+        console.log(`put didn't work`, error)
+      })
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios.delete(`https://practiceapi.devmountain.com/api/posts?id=${id}`)
+      .then((reuslts) => {
+        this.setState({ posts: reuslts.data })
+      })
+      .catch((error) => {
+        console.log(`delete didn't work`)
+      })
   }
 
-  createPost() {
+  createPost(text) {
+    axios.post('https://practiceapi.devmountain.com/api/posts', { text })
+      .then((results) => {
+        this.setState({ posts: results.data })
+      })
+      .catch((error)=>{
+        console.log(`post didn't work`)
+      })
 
   }
 
   render() {
     const { posts } = this.state;
-
+    // let postDisplay = this.state.posts.map((post) => (
+    //   <Post 
+    //   updatePostFn={this.updatePost}
+    //   key={post.id}
+    //   text={post.text}
+    //   date={post.date}
+    //   id={post.id}
+    //   />
+    // ))
     return (
       <div className="App__parent">
         <Header />
 
         <section className="App__content">
 
-          <Compose />
-          
+          <Compose createPostFn={this.createPost}/>
+          {
+            this.state.posts.map((post) => (
+              <Post key={post.id}
+                text={post.text}
+                date={post.date}
+                id={post.id}
+                updatePostFn={this.updatePost}
+                deletePostFn={this.deletePost}
+                // createPostFn={this.createPost}
+              />))
+          }
+          {/* {postDisplay} */}
+
         </section>
       </div>
     );
@@ -52,3 +106,4 @@ class App extends Component {
 }
 
 export default App;
+
